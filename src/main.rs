1a1b8129b488a986d8ed2fbdc9dcf2b8b4b0774f
mahi25soni <msdraj02@protonmail.com>
2025-06-30 22:21:56 +0530

    create wallet done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use axum:: {
     routing::{get, post},
-    Router
+    Router,
+    Json,
+    response::IntoResponse
 };
 use serde::{Serialize, Deserialize};
+
+use solana_sdk::{
+  signature::{Keypair, Signer},
+};
+
+use bs58;
 
 
 
@@ -22,10 +30,38 @@ struct ErrorResponse {
 
 
 
-#[tokio::main]
+// 1st 1st api and required strucutures, couldn't create required file!
+#[derive(Serialize)]
+struct WalletResponse {
+    pubkey : String,
+    secret_key : String
+}
+pub async fn create_wallet() -> impl IntoResponse {
+    let keypair = Keypair::new();
+    let public_key = keypair.pubkey().to_string();
+    let private_key_in_bytes = keypair.to_bytes();
+    let final_private_key = bs58::encode(private_key_in_bytes).into_string();
 
+    let res = WalletResponse {
+        pubkey : public_key,
+        secret_key : final_private_key
+    };
+    Json(SuccessResponse {
+        success: true,
+        data: res,
+    })
+}
+
+
+// now wedo 2nd api stuff
+
+
+
+
+#[tokio::main]
 async fn main() {
-    let app = Router::new().route("/check-health", get(check_health));
+    let app = Router::new().route("/check-health", get(check_health))
+        .route("/keypair", post(create_wallet));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
