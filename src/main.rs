59b2e51320404744af16b3f8b2ac0527f68a85e6
mahi25soni <msdraj02@protonmail.com>
2025-06-30 21:53:00 +0530

    struct setup

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,28 @@ use axum:: {
     routing::{get, post},
     Router
 };
+use serde::{Serialize, Deserialize};
+
+
+
+
+#[derive(Serialize)]
+struct SuccessResponse<T> {
+    success: bool,
+    data: T,
+}
+
+#[derive(Serialize)]
+struct ErrorResponse {
+    success: bool,
+    error: String,
+}
+
+
+
 
 #[tokio::main]
+
 async fn main() {
     let app = Router::new().route("/check-health", get(check_health));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
