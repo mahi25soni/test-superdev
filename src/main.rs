1eb503fdcb7b1b735f37e57f34d1edd829962766
mahi25soni <msdraj02@protonmail.com>
2025-06-30 21:46:05 +0530

    check health api for server, and all required crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum:: {
+    routing::{get, post},
+    Router
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/check-health", get(check_health));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn check_health() -> String {
+    "The server is working at 3000".to_string()
 }
